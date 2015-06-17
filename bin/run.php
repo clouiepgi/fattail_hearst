c208219b3012f27070e316b23b67649f6f1b15fd
Trey Hyde <Trey.Hyde@pgi.com>
2015-06-17 10:52:44 -0700

    Sketch of field mappsings

--- bin/run.php
+++ bin/run.php
@@ -10,6 +10,7 @@ use Symfony\Component\Console\Application;
 $container = new ContainerBuilder();
 $loader    = new YamlFileLoader($container, new FileLocator(__DIR__ . "/../config"));
 $loader->load('config.yml');
+$loader->load('field_mappings.yml');
 
 /** @var Application $app */
 $app = $container->get('console.application');
